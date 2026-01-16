2251afe4cf1b452d38851dc3d63aae4b25dc7af1
Peter Fausbøll <peter.fausboll@icloud.com>
2026-01-16 10:00:21 +0100

    4 commit i model-branch

--- Hydra_test.py
+++ Hydra_test.py
@@ -5,8 +5,8 @@ from omegaconf import DictConfig  # ja, den her er “OmegaConf”, men kun som 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(cfg: DictConfig):
     # Print kun det, I vil se (ingen config-dump)
-    print("env.data_root:", cfg.env.paths.data_root)
-    print("runtime.device:", cfg.env.runtime.device)
+    print("env.data_root:", cfg.config_env.paths.data_root)
+    print("runtime.device:", cfg.config_env.runtime.device)
     print("model:", cfg.model.name)
     print("epochs:", cfg.training.epochs)
 
