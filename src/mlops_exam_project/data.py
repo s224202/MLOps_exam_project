f1e9a37727d9bea0ba7ae78b188834134ef2273f
Adam <s224202@student.dtu.dk>
2026-01-21 20:03:41 +0100

    Fixed missing import

--- src/mlops_exam_project/data.py
+++ src/mlops_exam_project/data.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from torch.utils.data import Dataset
 import torch
-
+import typer
 import pandas as pd
 from ucimlrepo import fetch_ucirepo
 import os
@@ -15,8 +15,9 @@ class WineData(Dataset):
         self.data_path = data_path
         if not self.data_path.exists():
             if download:
-
-                print(f"Data file not found at {self.data_path}. Downloading from UCI ML Repository...")
+                print(
+                    f"Data file not found at {self.data_path}. Downloading from UCI ML Repository..."
+                )
 
                 wine_quality = fetch_ucirepo(id=186)
                 print(wine_quality.data.keys())
@@ -35,7 +36,7 @@ class WineData(Dataset):
                     f"Data file not found at {self.data_path}. If you want to download it, run with '--download'"
                 )
         self.data = pd.read_csv(self.data_path)
-    
+
     def __len__(self) -> int:
         """Return the length of the dataset."""
         return len(self.data)
@@ -52,9 +53,10 @@ class WineData(Dataset):
             target, dtype=torch.float32
         )
 
+        return torch.tensor(features, dtype=torch.float32), torch.tensor(
+            target, dtype=torch.long
+        )
 
-        return torch.tensor(features, dtype=torch.float32), torch.tensor(target, dtype=torch.long)
-    
     def preprocess(self, output_folder: Path) -> None:
         """Preprocess the raw data to have zero mean and unit variance and save it to the output folder."""
         processed_data = self.data.copy()
@@ -79,20 +81,32 @@ class WineData(Dataset):
         print(f"Processed data saved to {output_path}")
 
 
-def split_data(data, data_path,train_test_split_ratio: float = 0.8, train_val_split_ratio: float = 0.9) -> None:
+def split_data(
+    data,
+    data_path,
+    train_test_split_ratio: float = 0.8,
+    train_val_split_ratio: float = 0.9,
+) -> None:
     print("Splitting data into train, test, and validation sets...")
     print(f"Train/Test split ratio: {train_test_split_ratio}")
     print(f"Train/Validation split ratio: {train_val_split_ratio}")
 
-
-
-    train_data, test_data = train_test_split(data, test_size=1-train_test_split_ratio, random_state=42, stratify=data["quality"])
-    train_data, val_data = train_test_split(train_data, test_size=1-train_val_split_ratio, random_state=42, stratify=train_data["quality"]) 
+    train_data, test_data = train_test_split(
+        data,
+        test_size=1 - train_test_split_ratio,
+        random_state=42,
+        stratify=data["quality"],
+    )
+    train_data, val_data = train_test_split(
+        train_data,
+        test_size=1 - train_val_split_ratio,
+        random_state=42,
+        stratify=train_data["quality"],
+    )
 
     train_data = train_data.reset_index(drop=True)
     test_data = test_data.reset_index(drop=True)
     val_data = val_data.reset_index(drop=True)
-
 
     print(f"Train data size: {len(train_data)}")
     print(f"Test data size: {len(test_data)}")
@@ -100,28 +114,37 @@ def split_data(data, data_path,train_test_split_ratio: float = 0.8, train_val_sp
 
     print("Saving split data...")
     train_data.to_csv(data_path / "train_data.csv", index=False)
-    test_data.to_csv(data_path / "test_data.csv", index=False) 
-    val_data.to_csv(data_path / "val_data.csv", index=False)   
+    test_data.to_csv(data_path / "test_data.csv", index=False)
+    val_data.to_csv(data_path / "val_data.csv", index=False)
 
     print(f"Train, test, and validation data saved to {data_path}")
-    print("Data splitting completed.")     
-
+    print("Data splitting completed.")
 
 
 def preprocess(
     data_path: Path = typer.Option(Path("data/raw/Wqt.csv"), help="Path to raw data"),
-    output_folder: Path = typer.Option(Path("data/processed/"), help="Output folder for processed data"),
+    output_folder: Path = typer.Option(
+        Path("data/processed/"), help="Output folder for processed data"
+    ),
     download: bool = typer.Option(True, help="Download data if not found"),
     train_test_split_ratio: float = typer.Option(0.8, help="Train/test split ratio"),
-    train_val_split_ratio: float = typer.Option(0.9, help="Train/validation split ratio")
+    train_val_split_ratio: float = typer.Option(
+        0.9, help="Train/validation split ratio"
+    ),
 ) -> None:
-#def preprocess(data_path: Path = Path("data/raw/Wqt.csv"), output_folder: Path = Path("data/processed/"), download: bool = True, train_test_split_ratio: float = 0.8, train_val_split_ratio: float = 0.9) -> None:
-# def preprocess(data_path: Path, output_folder: Path, download: bool = False, train_test_split_ratio: float = 0.8, train_val_split_ratio: float = 0.9) -> None:
+    # def preprocess(data_path: Path = Path("data/raw/Wqt.csv"), output_folder: Path = Path("data/processed/"), download: bool = True, train_test_split_ratio: float = 0.8, train_val_split_ratio: float = 0.9) -> None:
+    # def preprocess(data_path: Path, output_folder: Path, download: bool = False, train_test_split_ratio: float = 0.8, train_val_split_ratio: float = 0.9) -> None:
     print("Preprocessing data...")
     dataset = WineData(data_path, download=download)
     dataset.preprocess(output_folder)
     print("Splitting data...")
-    split_data(dataset.data, output_folder, train_test_split_ratio=train_test_split_ratio, train_val_split_ratio=train_val_split_ratio)
+    split_data(
+        dataset.data,
+        output_folder,
+        train_test_split_ratio=train_test_split_ratio,
+        train_val_split_ratio=train_val_split_ratio,
+    )
+
 
 if __name__ == "__main__":
     print("Starting data preprocessing...")
@@ -132,9 +155,9 @@ if __name__ == "__main__":
     #     download=False,
     #     train_test_split_ratio=0.8,
     #     train_val_split_ratio=0.9
-    # )       
+    # )
     print("Data preprocessing completed.")
-    
+
 
 # Windows terminal command to run the script:
 # ./src/mlops_exam_project/data.py ./data/raw/Wqt.csv ./data/processed/ --download
